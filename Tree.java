8d881bc1f26f3d9ef395982ce2eea0c483f15120
ArturoAcosta <54242383+ArturoAcosta@users.noreply.github.com>
2021-02-10 15:52:00 -0600

    Update Tree.java
    Comentarios

--- Tree.java
+++ Tree.java
@@ -3,11 +3,8 @@ import java.io.File;
 public class Tree {
 
   /**
-  * Pretty print the directory tree and its file names.
-  *
-  * @param folder
-  *            must be a folder.
-  * @return
+  * El método imprime el arbol con las carpetas y archivos que contiene.
+  * @param folder debe ser una carpeta.
   */
   public String printDirectoryTree(File folder) {
       if (!folder.isDirectory()) {
@@ -19,6 +16,12 @@ public class Tree {
       return sb.toString();
   }
 
+  /**
+  * El método sirve para crear el string que se imprime en el método anterior.
+  * @param folder debe ser una carpeta.
+  * @param indent es para las filas que se terminaran imprimiendo
+  * @param sb es el StringBuilder que maneja todo lo que se imprimirá
+  */
   private void printDirectoryTree(File folder, int indent, StringBuilder sb) {
       if (!folder.isDirectory()) {
           throw new IllegalArgumentException("folder is not a Directory");
@@ -37,7 +40,12 @@ public class Tree {
       }
 
   }
-
+  /**
+  *El método agrega las partes necesarias para el formato de impresión y las agrega al *stringbuilder, llama al metodo getIndentSrting 
+  * @param folder debe ser una carpeta.
+  * @param indent es para las filas que se terminaran imprimiendo
+  * @param sb es el StringBuilder que maneja todo lo que se imprimirá
+  */
   private void printFile(File file, int indent, StringBuilder sb) {
       sb.append(getIndentString(indent));
       sb.append("+--");
@@ -45,6 +53,10 @@ public class Tree {
       sb.append("\n");
   }
 
+/**
+*Elmetodo agrega agrega las filas necesarias para todos los archivos y carpetas. en el *directorio
+* @param indent es para las filas que se terminaran imprimiendo
+*/
   private String getIndentString(int indent) {
       StringBuilder sb = new StringBuilder();
       for (int i = 0; i < indent; i++) {
